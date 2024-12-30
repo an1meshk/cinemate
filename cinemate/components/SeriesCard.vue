2d16fedd249007efd0383d2b2a7bac1c0cccd397
an1meshk <an1meshk@users.noreply.github.com>
2024-12-29 23:51:18 -0700

    update

--- cinemate/components/SeriesCard.vue
+++ cinemate/components/SeriesCard.vue
@@ -15,7 +15,7 @@
                 </p>
                 <p v-else
                     class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    No overview available
+                    No Overview Available
                 </p>
             </div>
         </div>
